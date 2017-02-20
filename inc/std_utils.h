f754e91eaa830e6b7116948df6a8a028a3a3d8ad
karatekid <mchristen96@gmail.com>
2017-02-20 15:01:04 -0800

    Add header guards and c linkage

--- inc/std_utils.h
+++ inc/std_utils.h
@@ -1,6 +1,19 @@
+#ifndef __STD_UTILS_H
+#define __STD_UTILS_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include <stdint.h>
 
 /* Get the string value of a uint32_t.
  * Required: c_str must be able to fit the resulting value.
  */
 void itoa(uint32_t val, char *c_str);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  // __STD_UTILS_H
--- inc/usart.h
+++ inc/usart.h
@@ -1,3 +1,10 @@
+#ifndef __USART_H
+#define __USART_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include <stdint.h>
 
 
@@ -15,3 +22,9 @@ void usart_configure(uint32_t baud_rate);
 char usart_block_receive_char();
 void usart_send_char(char c);
 void usart_send_string(const char *c_str);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  // __USART_H
